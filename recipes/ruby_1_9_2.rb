673c65f2a1a04109daa8f7bd4d911701f3da0845
Ruben Koster <ruben.koster@innovationfactory.eu>
2012-11-19 14:29:30 +0100

    Added readline package before ruby compile [fix #39777179]

--- recipes/ruby_1_9_2.rb
+++ recipes/ruby_1_9_2.rb
@@ -1,5 +1,7 @@
 include_recipe "rbenv"
 include_recipe "rbenv::ruby_build"
 
+# Compile ruby with readline support
+package "libreadline-dev"
 rbenv_ruby node['cloudfoundry_common']['ruby_1_9_2_version']
 
